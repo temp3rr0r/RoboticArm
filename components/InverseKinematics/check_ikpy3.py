92cde76093e229d1b2865d4319fb565ceb2fa23e
temp3rr0r <madks@yahoo.gr>
2019-04-25 12:00:50 +0200

    5 links works. Before limiting angles

--- components/InverseKinematics/check_ikpy3.py
+++ components/InverseKinematics/check_ikpy3.py
@@ -5,60 +5,54 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 from ikpy import geometry_utils
 
-# left_arm_chain = Chain(name='left_arm', links=[
-#     OriginLink(),
-#     URDFLink(
-#       name="shoulder",
-#       # translation_vector=[-10, 0, 5],
-#       translation_vector=[-1, 0, 0.5],
-#       orientation=[0, 1.57, 0],
-#       rotation=[0, 1, 0],
-#     ),
-#     URDFLink(
-#       name="elbow",
-#       # translation_vector=[25, 0, 0],
-#       translation_vector=[2.5, 0, 0],
-#       orientation=[0, 0, 0],
-#       rotation=[0, 1, 0],
-#     ),
-#     URDFLink(
-#       name="wrist",
-#       # translation_vector=[22, 0, 0],
-#       translation_vector=[2.2, 0, 0],
-#       orientation=[0, 0, 0],
-#       rotation=[0, 1, 0],
-#     )
-# ])
 
-init = np.array([0.0, 0.0, 0.0])
-link1 = np.array([0, 0.0, 0.1])
-link2 = np.array([0, 0.0, 0.12])
-link3 = np.array([0, 0.0, 0.9])
-link4 = np.array([0, 0.0, 0.6])
-link5 = np.array([0, 0.0, 0.12])
+# Link lengths in centimeters
+link1 = np.array([0, 0, 10.0])
+link2 = np.array([0, 0, 12.0])
+link3 = np.array([0, 0, 9.0])
+link4 = np.array([0, 0, 6.0])
+link5 = np.array([0, 0, 12.0])
+
+scale = 0.04
+
+# Joint rotation axis
+rotation1 = np.array([0, 0, 1])
+rotation2 = np.array([0, 1, 0])
+rotation3 = np.array([0, 1, 0])
+rotation4 = np.array([0, 1, 0])
+rotation5 = np.array([0, 0, 1])
 
 left_arm_chain = Chain(name='left_arm', links=[
     OriginLink(),
     URDFLink(
       name="shoulder",
-      # translation_vector=[-10, 0, 5],
-      translation_vector=link1,
+      translation_vector=link1 * scale,
       orientation=[0, 0, 0],
-      rotation=[0, 1, 0],
+      rotation=rotation1
     ),
     URDFLink(
       name="elbow",
-      # translation_vector=[25, 0, 0],
-      translation_vector=link2,
+      translation_vector=link2 * scale,
       orientation=[0, 0, 0],
-      rotation=[0, 1, 0],
+      rotation=rotation2,
     ),
     URDFLink(
       name="wrist",
-      # translation_vector=[22, 0, 0],
-      translation_vector=link3,
+      translation_vector=link3 * scale,
       orientation=[0, 0, 0],
-      rotation=[0, 1, 0],
+      rotation=rotation3
+    ),
+    URDFLink(
+      name="wrist",
+      translation_vector=link4 * scale,
+      orientation=[0, 0, 0],
+      rotation=rotation4
+    ),
+    URDFLink(
+      name="wrist",
+      translation_vector=link5 * scale,
+      orientation=[0, 0, 0],
+      rotation=rotation5
     )
 ])
 
@@ -74,7 +68,7 @@ left_arm_chain.plot(left_arm_chain.inverse_kinematics(geometry_utils.to_transfor
 matplotlib.pyplot.show()
 
 
-target_position = [0.75, 0.75, 0.75]
+target_position = [0.5, 0.5, 0.5]
 print("Target position: ", left_arm_chain.inverse_kinematics(geometry_utils.to_transformation_matrix(
     target_position,
     np.eye(3))))
@@ -82,7 +76,5 @@ ax = matplotlib.pyplot.figure().add_subplot(111, projection='3d')
 
 left_arm_chain.plot(left_arm_chain.inverse_kinematics(geometry_utils.to_transformation_matrix(
     target_position,
-    [[1, 0, 0],
-    [0, 1, 0],
-    [0, 0, 1]])), ax, target=target_position)
+    np.eye(3))), ax, target=target_position)
 matplotlib.pyplot.show()
