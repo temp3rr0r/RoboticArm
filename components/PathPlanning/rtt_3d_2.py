09ab7580cee8e55af928cee7a3afc6b945946a91
temp3rr0r <madks@yahoo.gr>
2019-05-06 14:10:38 +0200

    Widened and heightened boundaries around target

--- components/PathPlanning/rtt_3d_2.py
+++ components/PathPlanning/rtt_3d_2.py
@@ -14,9 +14,9 @@ obstacles = [(0.0, 0.0, 0.0, 2.2, 2.2, 2.2)]
 
 cube_side = 2.2
 center = (0, 0, 20)  # starting location
-target = (-10, -15, cube_side / 2.0)  # goal location
-boundary_distance = cube_side
-boundary_height = cube_side * 2.5
+target = (-20, -15, cube_side / 2.0)  # goal location
+boundary_distance = cube_side * 1
+boundary_height = cube_side * 0.5
 
 target_obstacles = [
     # (target[0] - 2 * d, target[1] - 2 * d, 0, target[0] + 2 * d, target[1] - d, 2 * cube_side),
@@ -29,10 +29,10 @@ target_obstacles = [
     # (target[0] - 2 * d, target[1] - 2 * d, 0.0, target[0] - 1 * d, target[1] + 2 * d, 2 * cube_side),
     # (target[0] + 2 * d, target[1] - 2 * d, 0.0, target[0] + 3 * d, target[1] + 2 * d, 2 * cube_side)
 
-    (target[0] - 1 * boundary_distance, target[1] - 2 * boundary_distance, 0, target[0] + 1 * boundary_distance, target[1] - boundary_distance, boundary_height),
-    (target[0] - 1 * boundary_distance, target[1] + 1 * boundary_distance, 0.0, target[0] + 1 * boundary_distance, target[1] + 2 * boundary_distance, boundary_height),
-    (target[0] - 2 * boundary_distance, target[1] - 1 * boundary_distance, 0.0, target[0] - 1 * boundary_distance, target[1] + 1 * boundary_distance, boundary_height),
-    (target[0] + 1 * boundary_distance, target[1] - 1 * boundary_distance, 0.0, target[0] + 2 * boundary_distance, target[1] + 1 * boundary_distance, boundary_height)
+    (target[0] - 2 * boundary_distance, target[1] - 2 * boundary_distance, 0, target[0] + 2 * boundary_distance, target[1] - boundary_distance, boundary_height),
+    (target[0] - 2 * boundary_distance, target[1] + 1 * boundary_distance, 0.0, target[0] + 2 * boundary_distance, target[1] + 2 * boundary_distance, boundary_height),
+    (target[0] - 2 * boundary_distance, target[1] - 2 * boundary_distance, 0.0, target[0] - 1 * boundary_distance, target[1] + 2 * boundary_distance, boundary_height),
+    (target[0] + 1 * boundary_distance, target[1] - 2 * boundary_distance, 0.0, target[0] + 2 * boundary_distance, target[1] + 2 * boundary_distance, boundary_height)
 ]
 print(target_obstacles)
 
@@ -51,7 +51,7 @@ Q = np.array([(r, r)])  # length of tree edges
 # Q = np.array([(8, 4)])
 
 
-max_samples = 512  # max number of samples to take before timing out
+max_samples = 1024  # max number of samples to take before timing out
 prc = 0.1  # probability of checking for a connection to goal
 rewire_count = 32  # optional, number of nearby branches to rewire
 
@@ -60,23 +60,23 @@ X = SearchSpace(X_dimensions, Obstacles)
 rrt = RRTStar(X, Q, center, target, max_samples, r, prc, rewire_count)
 path = rrt.rrt_star()
 
-print(path)
-
-# from mpl_toolkits.mplot3d import axes3d
-# import matplotlib.pyplot as plt
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# X, Y, Z = zip(*path)
-# ax.plot(X, Y, Z)
-# plt.show()
-
-# plot
-plot = Plot("rrt_star_3d")
-plot.filename = "asdf.html"
-plot.plot_tree(X, rrt.trees)
 if path is not None:
-    plot.plot_path(X, path)
-plot.plot_obstacles(X, Obstacles)
-plot.plot_start(X, center)
-plot.plot_goal(X, target)
-plot.draw(auto_open=True)
+    print(path)
+    # from mpl_toolkits.mplot3d import axes3d
+    # import matplotlib.pyplot as plt
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # X, Y, Z = zip(*path)
+    # ax.plot(X, Y, Z)
+    # plt.show()
+
+    # plot
+    plot = Plot("rrt_star_3d")
+    plot.filename = "asdf.html"
+    plot.plot_tree(X, rrt.trees)
+    if path is not None:
+        plot.plot_path(X, path)
+    plot.plot_obstacles(X, Obstacles)
+    plot.plot_start(X, center)
+    plot.plot_goal(X, target)
+    plot.draw(auto_open=True)
